9290e7557f48e931c4fd48e984924e8c94333a26
Sergio Solis <solis.sergioariel@gmail.com>
2026-01-06 00:40:55 -0300

    fixing errors deploy

--- src/providers/privy/index.tsx
+++ src/providers/privy/index.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { PrivyProvider as Privy } from '@privy-io/react-auth';
-import {toSolanaWalletConnectors} from "@privy-io/react-auth/solana";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
 
 export function PrivyProvider({ children }: { children: React.ReactNode }) {
     return (
         <Privy
             appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
-            clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID as string}
+            clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT as string}
             config={{
-                solanaClusters: [{name: 'devnet', rpcUrl: 'https://api.devnet.solana.com'}],
+                solanaClusters: [{ name: 'devnet', rpcUrl: 'https://api.devnet.solana.com' }],
                 // Customize Privy's appearance in your app
                 appearance: {
                     theme: 'light',
